fb519443a336947229baad4c7638c766519a0733
satoyan <satoyan.discord@gmail.com>
2024-01-17 00:01:37 +0900

    [add] File update detection function added

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,8 @@ class Main {
 		//configはトークンを含むためthisに入れない
 		const config = <configType>dotenv_config().parsed;
 		if (!config) throw Error(".env config could not be loaded.");
+		console.log(config);
+		console.log("Successfully loaded .env!");
 		this.latest_log_path = path.join(config.latest_log_path);
 		this.logWatch = new logWatch(this.latest_log_path);
 	}
@@ -29,20 +31,49 @@ class logWatch {
 	latest_log_path: string;
 	event: EventEmitter;
 	on: (eventName: string | symbol, listener: (...args: any[]) => void) => EventEmitter;
+	check_interval_ms: number;
+	last_size: number;
+	last_text: string;
 	constructor(path: string) {
 		this.latest_log_path = path;
+		this.check_interval_ms = 1000; //チェック間隔(ms)
 		this.event = new EventEmitter();
-		this.on = this.event.on;
-		this.watchLogfile();
+		this.checkLogfile();
+		console.log("watch log file path:" + this.latest_log_path);
+		this.event.on("change", this.chengeEventAfter.bind(this));
+
+		this.last_size = 0;
+		this.last_text = "";
 	}
 
-	private watchLogfile() {
-		const FSwatcher = fs.watch(this.latest_log_path);
-		FSwatcher.on("change", (event_type, filename) => {
-			fs.readFile(this.latest_log_path, (err, data) => {
-				console.log(data);
+	private checkLogfile() {
+		const check = () => {
+			const size = fs.statSync(this.latest_log_path).size;
+			console.log(this.last_size, size);
+			if (this.last_size < size && this.last_size !== 0) {
+				this.event.emit("change");
+			}
+			this.last_size = size;
+		};
+		setInterval(check, this.check_interval_ms);
+	}
+	private chengeEventAfter() {
+		const stream = fs.createReadStream(this.latest_log_path);
+		let chunks: Buffer[] = [];
+		stream
+			.on("data", (chunk: Buffer) => {
+				chunks.push(chunk);
+			})
+			.on("end", () => {
+				const buffer = Buffer.concat(
+					chunks,
+					chunks.reduce((sum, chunk) => (sum += chunk.length), 0)
+				);
+				const text = buffer.toString("utf-8");
+				const split_text = text.split(this.last_text);
+				this.event.emit("change_diff", split_text);
+				this.last_text = text;
 			});
-		});
 	}
 }
 
